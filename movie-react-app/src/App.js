02cfcfaee0c38d1580b657c16a86de11b4b49e2e
Hena-yaris <meraey21@gmail.com>
2024-02-18 04:59:47 -0800

    home page and header

--- movie-react-app/src/App.js
+++ movie-react-app/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Header from './Components/Header/Header';
+import Home from  './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
-          <Route index element={<h1>Hello there </h1>} />
+          <Route index element={<Home/>} />
           <Route path='movie/:id' element={<h1>movie detail page </h1>} />
           <Route path='movies/:type' element={<h1>movie list page</h1>} />
-          <Route path='*' element={<h1>page not found 404</h1>} />
+          <Route path='/*' element={<h1>page not found 404</h1>} />
         </Routes>
       </Router>
     </div>
---
+++ movie-react-app/src/Components/Header/Header.js
@@ -0,0 +1,32 @@
+
+import './header.css';
+import { Link } from 'react-router-dom';
+import Eva_Logo from "../../images/10002.jpg";
+
+const Header= () => {
+
+
+    return (
+      <>
+        <div className="header">
+          <div className="headerLeft">
+            <Link to="/">
+              <img className="header__icon" src={Eva_Logo} />
+            </Link>
+            <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+              <span>Popular</span>
+            </Link>
+            <Link to="/movies/top_rated" style={{ textDecoration: "none" }}>
+              <span>Top Rated</span>
+            </Link>
+            <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+              <span>Upcoming</span>
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+}
+
+
+export default Header;
