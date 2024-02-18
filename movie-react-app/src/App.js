aa797c0ceb8091a43ab5ffa8a7cffd5be80fc53e
Hena-yaris <meraey21@gmail.com>
2024-02-18 03:16:22 -0800

    create routes

--- movie-react-app/src/App.js
+++ movie-react-app/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello there </h1>
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello there </h1>} />
+          <Route path='movie/:id' element={<h1>movie detail page </h1>} />
+          <Route path='movies/:type' element={<h1>movie list page</h1>} />
+          <Route path='*' element={<h1>page not found 404</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
